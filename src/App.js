a3aafe60f95d0d2baf4d838d28f79b6934602d3c
으니 <72530436+eundol0519@users.noreply.github.com>
2024-04-05 13:00:48 +0900

    📰 : Function Component Practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let counter = 0;
+  const [stateCounter, setStateCounter] = useState(0);
+
+  const increase = () => {
+    counter = counter + 1;
+    setStateCounter((prev) => prev + 1);
+    console.log(`counter : ${counter}, stateCounter : ${stateCounter}`);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{counter}</div>
+      <div>state : {stateCounter}</div>
+      <button onClick={increase}>Click</button>
     </div>
   );
 }

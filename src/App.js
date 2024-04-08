0f1e80a0395695f0ab1c51f6f9ff296ac7cba11a
으니 <72530436+eundol0519@users.noreply.github.com>
2024-04-08 17:59:44 +0900

    📝 : Function Component Lifecycle

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
 
 function App() {
   let counter = 0;
@@ -10,6 +10,20 @@ function App() {
     setStateCounter((prev) => prev + 1);
     console.log(`counter : ${counter}, stateCounter : ${stateCounter}`);
   }
+
+  // DOM이 그려지기 전에 실행됨
+  useLayoutEffect(() => {
+    console.log("useLayoutEffect")
+  })
+
+  // DOM이 그려진 후에 실행됨
+  useEffect(() => {
+    console.log("mounting")
+
+    return () => {
+      console.log("unmounting")
+    }
+  }, []) // updating
 
   return (
     <div>
